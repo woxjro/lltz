bf5cc4ca1c6f9dc87b9183605858fe242433b9f7
woxjro <usuzawa.yoshi.55z@st.kyoto-u.ac.jp>
2023-02-19 23:00:11 +0900

    [refactor](backend): スタック初期化処理をmichelson-astで書き換え

--- src/val.rs
+++ src/val.rs
@@ -36,7 +36,7 @@ impl Val {
     pub fn to_string(&self) -> String {
         match self {
             Val::Address(addr) => addr.clone(),
-            Val::Bool(b) => b.to_string(),
+            Val::Bool(b) => if *b { "True" } else { "False" }.to_string(),
             Val::Int(i) => i.to_string(),
             Val::Mutez(m) => m.to_string(),
             Val::Nat(n) => n.to_string(),

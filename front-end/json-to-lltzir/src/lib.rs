10a2711725136fabd5445fff9c3ccec08b63919e
woxjro <usuzawa.yoshi.55z@st.kyoto-u.ac.jp>
2023-03-04 18:56:24 +0900

    [feat](json-to-lltzir): whileの基本ブロック中に命令列を出力した

--- front-end/json-to-lltzir/src/lib.rs
+++ front-end/json-to-lltzir/src/lib.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+use serde_json::Value;
 use String as Label;
 use String as Register;
 
@@ -16,12 +17,47 @@ pub struct Cfg {
 }
 
 impl Cfg {
-    pub fn print(&self) {
-        println!("while {{ {} }} (%{}) {{", self.header, self.header_cond);
+    fn instructions_to_string(instructions: &Value) -> String {
+        let mut res = "".to_string();
+        match instructions {
+            Value::Array(instructions) => {
+                for instruction in instructions {
+                    if !instruction["original_instruction"]
+                        .to_string()
+                        .contains("br")
+                    {
+                        let s = instruction["original_instruction"].to_string();
+                        let r = s.trim_matches(|c| c == ' ' || c == '"');
+
+                        res = format!("{res}\n{r}");
+                    }
+                }
+            }
+            e => {
+                panic!("{:?}", e)
+            }
+        };
+
+        format!("{res}\n")
+    }
+    pub fn print(&self, lltz_ir: &Value) {
+        let blocks = &lltz_ir["functions"][0]["blocks"];
+        println!(
+            "while {{ {} }} (%{}) {{",
+            Cfg::instructions_to_string(&blocks[self.header.clone()]["instructions"]),
+            self.header_cond
+        );
         for lp in &self.sub_loops {
-            lp.print();
+            lp.print(lltz_ir);
         }
-        println!("  {}", self.latch);
-        println!("}} {}", self.exiting);
+        println!(
+            "  {}",
+            Cfg::instructions_to_string(&blocks[self.latch.clone()]["instructions"])
+        );
+        println!("}}");
+        println!(
+            "{}",
+            Cfg::instructions_to_string(&blocks[self.exiting.clone()]["instructions"])
+        );
     }
 }

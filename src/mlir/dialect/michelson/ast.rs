32f4aaee3b4ae1c2a5aa4445fa26c43cc2936bb2
woxjro <usuzawa.yoshi.55z@st.kyoto-u.ac.jp>
2023-05-13 21:29:20 +0900

    [fix,feat](lltz:mlir): Pair の 要素を ty1, ty2 へと変更．Option を追加

--- src/mlir/dialect/michelson/ast.rs
+++ src/mlir/dialect/michelson/ast.rs
@@ -10,9 +10,12 @@ pub enum Type {
     Unit,
     Mutez,
     Operation,
+    Option {
+        ty: Box<Type>,
+    },
     Pair {
-        fst: Box<Type>,
-        snd: Box<Type>,
+        ty1: Box<Type>,
+        ty2: Box<Type>,
     },
     List {
         ty: Box<Type>,
@@ -25,14 +28,15 @@ pub enum Type {
 }
 
 impl Type {
+    //FIXME: 削除したい．impl From<Type> for MTy としたい
     pub fn michelify(&self) -> MTy {
         match self {
             Type::Unit => MTy::Unit,
             Type::Mutez => MTy::Mutez,
             Type::Operation => MTy::Operation,
-            Type::Pair { fst, snd } => MTy::Pair {
-                ty1: Box::new(fst.michelify()),
-                ty2: Box::new(snd.michelify()),
+            Type::Pair { ty1, ty2 } => MTy::Pair {
+                ty1: Box::new(ty1.michelify()),
+                ty2: Box::new(ty2.michelify()),
             },
             Type::List { ty } => MTy::List {
                 ty: Box::new(ty.michelify()),
@@ -40,6 +44,7 @@ impl Type {
             _ => todo!(),
         }
     }
+
     pub fn get_dialect(&self) -> dialect::DialectKind {
         dialect::DialectKind::Michelson
     }

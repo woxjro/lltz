f2bfb3e78dfddfab891520405d72cbce23fcb7cf
woxjro <usuzawa.yoshi.55z@st.kyoto-u.ac.jp>
2023-05-13 21:54:36 +0900

    [fix](test): use Type::from

--- tests/mlir_parser.rs
+++ tests/mlir_parser.rs
@@ -1,32 +1,28 @@
-use lltz::json;
 use lltz::mlir::dialect::michelson::ast::Type;
 #[test]
 fn mlir_parser() {
+    assert_eq!(Type::from("!michelson.mutez".to_owned()), Type::Mutez);
     assert_eq!(
-        json::to_mlir::string_to_mlir("!michelson.mutez".to_owned(),),
-        Type::Mutez
-    );
-    assert_eq!(
-        json::to_mlir::string_to_mlir("!michelson.list<!michelson.mutez>".to_owned(),),
+        Type::from("!michelson.list<!michelson.mutez>".to_owned(),),
         Type::List {
             ty: Box::new(Type::Mutez)
         }
     );
 
     assert_eq!(
-        json::to_mlir::string_to_mlir(
+        Type::from(
             "!michelson.pair<!michelson.list<!michelson.operation>,!michelson.mutez>".to_owned(),
         ),
         Type::Pair {
-            fst: Box::new(Type::List {
+            ty1: Box::new(Type::List {
                 ty: Box::new(Type::Operation)
             }),
-            snd: Box::new(Type::Mutez)
+            ty2: Box::new(Type::Mutez)
         }
     );
 
     assert_eq!(
-        json::to_mlir::string_to_mlir(
+        Type::from(
             "(!michelson.mutez, !michelson.mutez) -> !michelson.pair<!michelson.list<!michelson.operation>, !michelson.mutez>".to_owned(),
         ),
         Type::SmartContract {
@@ -34,24 +30,24 @@ fn mlir_parser() {
             storage: Box::new(Type::Mutez),
             res: Box::new(
                 Type::Pair {
-                    fst: Box::new(Type::List {
+                    ty1: Box::new(Type::List {
                         ty: Box::new(Type::Operation)
                     }),
-                    snd: Box::new(Type::Mutez)
+                    ty2: Box::new(Type::Mutez)
                 }
             )
         }
     );
 
     assert_eq!(
-        json::to_mlir::string_to_mlir(
+        Type::from(
             "!michelson.pair<!michelson.list<!michelson.operation>,!michelson.mutez>".to_owned(),
         ),
         Type::Pair {
-            fst: Box::new(Type::List {
+            ty1: Box::new(Type::List {
                 ty: Box::new(Type::Operation)
             }),
-            snd: Box::new(Type::Mutez)
+            ty2: Box::new(Type::Mutez)
         }
     );
 }

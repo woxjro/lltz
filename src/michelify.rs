ed612151a9ef011301449ac326e82e42af8efc4a
woxjro <usuzawa.yoshi.55z@st.kyoto-u.ac.jp>
2023-05-16 12:31:55 +0900

    [refactor](lltz:michelify): 各 phase の処理を phase.rs へと移動

--- src/michelify.rs
+++ src/michelify.rs
@@ -1,14 +1,10 @@
 mod ast;
-use crate::michelify::ast::StackType;
-use crate::mlir;
+mod phase;
 use crate::mlir::ast::{Operation, Value};
 use crate::mlir::dialect::michelson::ast::Type;
-use michelson_ast::instruction::Instruction as MichelsonInstruction;
-use michelson_ast::instruction_row;
 use michelson_ast::program;
-use michelson_ast::ty::Ty as MichelsonType;
-use michelson_ast::wrapped_instruction::WrappedInstruction as MWrappedInstr;
 use std::collections::HashMap;
+
 pub fn compile(smart_contract: Operation) -> String {
     /*
      * Value を Key として,その Value の Michelson の Stack 上での位置を返す HashMap
@@ -36,13 +32,13 @@ pub fn compile(smart_contract: Operation) -> String {
      */
     let mut type_heap_address_counter = 0;
 
-    let (parameter, storage) = get_signature(&smart_contract);
+    let (parameter, storage) = phase::get_signature(&smart_contract);
     let mut code = vec![];
 
     /*
      * scan operations
      */
-    scan(
+    phase::scan(
         &smart_contract,
         &mut value_address_counter,
         &mut type_heap_address_counter,
@@ -54,21 +50,21 @@ pub fn compile(smart_contract: Operation) -> String {
      * stack initialization
      */
     let mut stack_initialization_instructions =
-        stack_initialization(&value_addresses, &type_heap_addresses);
+        phase::stack_initialization(&value_addresses, &type_heap_addresses);
     code.append(&mut stack_initialization_instructions);
 
-    let get_address_closure = get_get_address_closure(value_addresses.clone());
+    let get_address_closure = phase::get_get_address_closure(value_addresses.clone());
     /*
      * compile operations
      */
     let mut compile_operations_instructions =
-        compile_operations(&smart_contract, get_address_closure.as_ref());
+        phase::compile_operations(&smart_contract, get_address_closure.as_ref());
     code.append(&mut compile_operations_instructions);
 
     /*
      * drop an unused stack region
      */
-    let mut exit_instructions = exit(
+    let mut exit_instructions = phase::exit(
         &smart_contract,
         value_addresses.iter().len() + type_heap_addresses.iter().len(),
         get_address_closure.as_ref(),
@@ -82,263 +78,4 @@ pub fn compile(smart_contract: Operation) -> String {
     };
 
     michelson_program.to_string()
-}
-
-fn get_signature(smart_contract: &Operation) -> (MichelsonType, MichelsonType) {
-    let args = smart_contract.regions[0].blocks[0].arguments.to_owned();
-    if args.len() == 2 {
-        let storage_v = args[0].get_value();
-        let param_v = args[1].get_value();
-        (
-            MichelsonType::from(storage_v.get_type()),
-            MichelsonType::from(param_v.get_type()),
-        )
-    } else {
-        panic!(
-            "A smart_contract function is being given {} arguments instead of 2.",
-            args.len()
-        )
-    }
-}
-
-fn scan(
-    smart_contract: &Operation,
-    value_address_counter: &mut usize,
-    _type_heap_address_counter: &mut usize,
-    value_addresses: &mut HashMap<Value, usize>,
-    _type_heap_addresses: &mut HashMap<Type, usize>,
-) {
-    let operations = smart_contract.regions[0].blocks[0].operations.to_owned();
-    for operation in operations {
-        //TODO: While や If のような内部に Region を持つ命令の場合は再帰的に scan する必要がある
-        for operand in operation.operands {
-            let _ = value_addresses
-                .entry(operand.get_value())
-                .or_insert_with(|| {
-                    *value_address_counter += 1;
-                    *value_address_counter
-                });
-        }
-
-        for result in operation.results {
-            let _ = value_addresses
-                .entry(result.get_value())
-                .or_insert_with(|| {
-                    *value_address_counter += 1;
-                    *value_address_counter
-                });
-        }
-    }
-    let args = smart_contract.regions[0].blocks[0].arguments.to_owned();
-    for arg in args {
-        let _ = value_addresses.entry(arg.get_value()).or_insert_with(|| {
-            *value_address_counter += 1;
-            *value_address_counter
-        });
-    }
-}
-
-///before:            (parameter, storage) |> stack_bottom
-///after : [value region] |> [heap region] |> stack_bottom
-fn stack_initialization(
-    value_addresses: &HashMap<Value, usize>,
-    type_heap_addresses: &HashMap<Type, usize>,
-) -> Vec<MWrappedInstr> {
-    let mut michelson_instructions = vec![
-        instruction_row!(MichelsonInstruction::Comment(format!(
-            "------ Stack Initialization ------ {{"
-        ))),
-        //TODO: 引数が Option で包まなければいけない型の場合の処理をする
-        instruction_row!(
-            MichelsonInstruction::Unpair,
-            format!("(parameter, storage) => param |> storage")
-        ),
-    ];
-
-    let mut type_heap_addresses = type_heap_addresses
-        .iter()
-        .map(|(k, v)| (k.clone(), v.clone()))
-        .collect::<Vec<_>>();
-    type_heap_addresses.sort_by(|a, b| (a.1).cmp(&b.1));
-
-    for (_ty, _address) in type_heap_addresses.iter().rev() {
-        todo!()
-    }
-
-    let mut value_addresses = value_addresses
-        .iter()
-        .map(|(k, v)| (k.clone(), v.clone()))
-        .collect::<Vec<_>>();
-    value_addresses.sort_by(|a, b| (a.1).cmp(&b.1));
-    for (i, (value, _address)) in value_addresses.iter().rev().enumerate() {
-        if i <= 1 {
-            continue; // parameter と storage の初期値の処理をスキップ
-        }
-        let stack_type: StackType = value.get_type().into();
-        michelson_instructions.push(instruction_row!(
-            stack_type.default_value_instruction(),
-            format!(
-                "{} : {}",
-                value.get_id(),
-                MichelsonType::from(value.get_type()).to_string()
-            )
-        ));
-    }
-
-    michelson_instructions.push(instruction_row!(MichelsonInstruction::Comment(format!(
-        "---------------------------------- }}"
-    ))));
-
-    michelson_instructions
-}
-
-///before: [value region] |> [heap region] |> stack_bottom
-///after : [value region] |> [heap region] |> stack_bottom
-fn compile_operations(
-    smart_contract: &Operation,
-    get_address_closure: &(dyn Fn(Value) -> usize),
-) -> Vec<MWrappedInstr> {
-    let mut instructions = vec![];
-
-    let operations = smart_contract.regions[0].blocks[0].operations.to_owned();
-
-    for operation in operations {
-        let operation = mlir::dialect::Operation::from(operation);
-        match operation {
-            mlir::dialect::Operation::FuncOp { operation } => {
-                use mlir::dialect::func;
-                match operation {
-                    func::ast::Operation::ReturnOp { .. } => {}
-                }
-            }
-            mlir::dialect::Operation::MichelsonOp { operation } => {
-                use mlir::dialect::michelson;
-                match operation {
-                    michelson::ast::Operation::GetUnitOp { result } => {
-                        let address = (*get_address_closure)(result.get_value());
-                        instructions.append(
-                            &mut vec![
-                                MichelsonInstruction::Comment(format!(
-                                    "{} = michelson.get_unit() {{",
-                                    result.get_value().get_id()
-                                )),
-                                MichelsonInstruction::Unit,
-                                MichelsonInstruction::DigN(address),
-                                MichelsonInstruction::Drop,
-                                MichelsonInstruction::DugN(address - 1),
-                                MichelsonInstruction::Comment("}".to_string()),
-                            ]
-                            .iter()
-                            .map(|instr| instr.to_wrapped_instruction())
-                            .collect::<Vec<_>>(),
-                        );
-                    }
-                    michelson::ast::Operation::GetAmountOp { result } => {
-                        let address = (*get_address_closure)(result.get_value());
-                        instructions.append(
-                            &mut vec![
-                                MichelsonInstruction::Comment(format!(
-                                    "{} = michelson.get_amount() {{",
-                                    result.get_value().get_id()
-                                )),
-                                MichelsonInstruction::Amount,
-                                MichelsonInstruction::DigN(address),
-                                MichelsonInstruction::Drop,
-                                MichelsonInstruction::DugN(address - 1),
-                                MichelsonInstruction::Comment("}".to_string()),
-                            ]
-                            .iter()
-                            .map(|instr| instr.to_wrapped_instruction())
-                            .collect::<Vec<_>>(),
-                        );
-                    }
-                    michelson::ast::Operation::MakeListOp { result } => {
-                        //スタック初期化の際に `nil ty` を既に積んでいるため何もする必要無し
-                        instructions.append(
-                            &mut vec![MichelsonInstruction::Comment(format!(
-                                "{} = michelson.make_list() {{ }}",
-                                result.get_value().get_id()
-                            ))]
-                            .iter()
-                            .map(|instr| instr.to_wrapped_instruction())
-                            .collect::<Vec<_>>(),
-                        )
-                    }
-                    michelson::ast::Operation::MakePairOp { result, fst, snd } => {
-                        let result_address = (*get_address_closure)(result.get_value());
-                        let fst_address = (*get_address_closure)(fst.get_value());
-                        let snd_address = (*get_address_closure)(snd.get_value());
-                        instructions.append(
-                            &mut vec![
-                                MichelsonInstruction::Comment(format!(
-                                    "{} = michelson.make_pair({}, {}) {{",
-                                    result.get_value().get_id(),
-                                    fst.get_value().get_id(),
-                                    snd.get_value().get_id(),
-                                )),
-                                MichelsonInstruction::DupN(snd_address),
-                                MichelsonInstruction::DupN(fst_address + 1),
-                                MichelsonInstruction::Pair,
-                                MichelsonInstruction::DigN(result_address),
-                                MichelsonInstruction::Drop,
-                                MichelsonInstruction::Some,
-                                MichelsonInstruction::DugN(result_address - 1),
-                                MichelsonInstruction::Comment("}".to_string()),
-                            ]
-                            .iter()
-                            .map(|instr| instr.to_wrapped_instruction())
-                            .collect::<Vec<_>>(),
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    instructions
-}
-
-///before: [value region] |> [heap region] |> stack_bottom
-///after :           (operations, storage) |> stack_bottom
-fn exit(
-    smart_contract: &Operation,
-    stack_size: usize,
-    get_address_closure: &(dyn Fn(Value) -> usize),
-) -> Vec<MWrappedInstr> {
-    let return_op = smart_contract.regions[0].blocks[0]
-        .operations
-        .last()
-        .unwrap()
-        .to_owned();
-    let value = return_op.operands[0].get_value();
-    let address = (*get_address_closure)(value);
-
-    let mut instructions = vec![instruction_row!(MichelsonInstruction::Comment(format!(
-        "-------------- Exit -------------- {{"
-    )))];
-
-    instructions.append(
-        &mut vec![
-            MichelsonInstruction::DigN(address - 1),
-            MichelsonInstruction::AssertSome,
-            MichelsonInstruction::DugN(stack_size - 1),
-        ]
-        .iter()
-        .map(|instr| instr.to_wrapped_instruction())
-        .collect::<Vec<_>>(),
-    );
-
-    for _ in 0..stack_size - 1 {
-        instructions.push(instruction_row!(MichelsonInstruction::Drop));
-    }
-
-    instructions.push(instruction_row!(MichelsonInstruction::Comment(format!(
-        "---------------------------------- }}"
-    ))));
-
-    instructions
-}
-
-fn get_get_address_closure(value_addresses: HashMap<Value, usize>) -> Box<dyn Fn(Value) -> usize> {
-    Box::new(move |value| *value_addresses.get(&value).unwrap())
 }
---
+++ src/michelify/phase.rs
@@ -0,0 +1,270 @@
+use crate::michelify::ast::StackType;
+use crate::mlir;
+use crate::mlir::ast::{Operation, Value};
+use crate::mlir::dialect::michelson::ast::Type;
+use michelson_ast::instruction::Instruction as MichelsonInstruction;
+use michelson_ast::instruction_row;
+use michelson_ast::ty::Ty as MichelsonType;
+use michelson_ast::wrapped_instruction::WrappedInstruction as MWrappedInstr;
+use std::collections::HashMap;
+
+pub fn get_signature(smart_contract: &Operation) -> (MichelsonType, MichelsonType) {
+    let args = smart_contract.regions[0].blocks[0].arguments.to_owned();
+    if args.len() == 2 {
+        let storage_v = args[0].get_value();
+        let param_v = args[1].get_value();
+        (
+            MichelsonType::from(storage_v.get_type()),
+            MichelsonType::from(param_v.get_type()),
+        )
+    } else {
+        panic!(
+            "A smart_contract function is being given {} arguments instead of 2.",
+            args.len()
+        )
+    }
+}
+
+pub fn scan(
+    smart_contract: &Operation,
+    value_address_counter: &mut usize,
+    _type_heap_address_counter: &mut usize,
+    value_addresses: &mut HashMap<Value, usize>,
+    _type_heap_addresses: &mut HashMap<Type, usize>,
+) {
+    let operations = smart_contract.regions[0].blocks[0].operations.to_owned();
+    for operation in operations {
+        //TODO: While や If のような内部に Region を持つ命令の場合は再帰的に scan する必要がある
+        for operand in operation.operands {
+            let _ = value_addresses
+                .entry(operand.get_value())
+                .or_insert_with(|| {
+                    *value_address_counter += 1;
+                    *value_address_counter
+                });
+        }
+
+        for result in operation.results {
+            let _ = value_addresses
+                .entry(result.get_value())
+                .or_insert_with(|| {
+                    *value_address_counter += 1;
+                    *value_address_counter
+                });
+        }
+    }
+    let args = smart_contract.regions[0].blocks[0].arguments.to_owned();
+    for arg in args {
+        let _ = value_addresses.entry(arg.get_value()).or_insert_with(|| {
+            *value_address_counter += 1;
+            *value_address_counter
+        });
+    }
+}
+
+///before:            (parameter, storage) |> stack_bottom
+///after : [value region] |> [heap region] |> stack_bottom
+pub fn stack_initialization(
+    value_addresses: &HashMap<Value, usize>,
+    type_heap_addresses: &HashMap<Type, usize>,
+) -> Vec<MWrappedInstr> {
+    let mut michelson_instructions = vec![
+        instruction_row!(MichelsonInstruction::Comment(format!(
+            "------ Stack Initialization ------ {{"
+        ))),
+        //TODO: 引数が Option で包まなければいけない型の場合の処理をする
+        instruction_row!(
+            MichelsonInstruction::Unpair,
+            format!("(parameter, storage) => param |> storage")
+        ),
+    ];
+
+    let mut type_heap_addresses = type_heap_addresses
+        .iter()
+        .map(|(k, v)| (k.clone(), v.clone()))
+        .collect::<Vec<_>>();
+    type_heap_addresses.sort_by(|a, b| (a.1).cmp(&b.1));
+
+    for (_ty, _address) in type_heap_addresses.iter().rev() {
+        todo!()
+    }
+
+    let mut value_addresses = value_addresses
+        .iter()
+        .map(|(k, v)| (k.clone(), v.clone()))
+        .collect::<Vec<_>>();
+    value_addresses.sort_by(|a, b| (a.1).cmp(&b.1));
+    for (i, (value, _address)) in value_addresses.iter().rev().enumerate() {
+        if i <= 1 {
+            continue; // parameter と storage の初期値の処理をスキップ
+        }
+        let stack_type: StackType = value.get_type().into();
+        michelson_instructions.push(instruction_row!(
+            stack_type.default_value_instruction(),
+            format!(
+                "{} : {}",
+                value.get_id(),
+                MichelsonType::from(value.get_type()).to_string()
+            )
+        ));
+    }
+
+    michelson_instructions.push(instruction_row!(MichelsonInstruction::Comment(format!(
+        "---------------------------------- }}"
+    ))));
+
+    michelson_instructions
+}
+
+///before: [value region] |> [heap region] |> stack_bottom
+///after : [value region] |> [heap region] |> stack_bottom
+pub fn compile_operations(
+    smart_contract: &Operation,
+    get_address_closure: &(dyn Fn(Value) -> usize),
+) -> Vec<MWrappedInstr> {
+    let mut instructions = vec![];
+
+    let operations = smart_contract.regions[0].blocks[0].operations.to_owned();
+
+    for operation in operations {
+        let operation = mlir::dialect::Operation::from(operation);
+        match operation {
+            mlir::dialect::Operation::FuncOp { operation } => {
+                use mlir::dialect::func;
+                match operation {
+                    func::ast::Operation::ReturnOp { .. } => {}
+                }
+            }
+            mlir::dialect::Operation::MichelsonOp { operation } => {
+                use mlir::dialect::michelson;
+                match operation {
+                    michelson::ast::Operation::GetUnitOp { result } => {
+                        let address = (*get_address_closure)(result.get_value());
+                        instructions.append(
+                            &mut vec![
+                                MichelsonInstruction::Comment(format!(
+                                    "{} = michelson.get_unit() {{",
+                                    result.get_value().get_id()
+                                )),
+                                MichelsonInstruction::Unit,
+                                MichelsonInstruction::DigN(address),
+                                MichelsonInstruction::Drop,
+                                MichelsonInstruction::DugN(address - 1),
+                                MichelsonInstruction::Comment("}".to_string()),
+                            ]
+                            .iter()
+                            .map(|instr| instr.to_wrapped_instruction())
+                            .collect::<Vec<_>>(),
+                        );
+                    }
+                    michelson::ast::Operation::GetAmountOp { result } => {
+                        let address = (*get_address_closure)(result.get_value());
+                        instructions.append(
+                            &mut vec![
+                                MichelsonInstruction::Comment(format!(
+                                    "{} = michelson.get_amount() {{",
+                                    result.get_value().get_id()
+                                )),
+                                MichelsonInstruction::Amount,
+                                MichelsonInstruction::DigN(address),
+                                MichelsonInstruction::Drop,
+                                MichelsonInstruction::DugN(address - 1),
+                                MichelsonInstruction::Comment("}".to_string()),
+                            ]
+                            .iter()
+                            .map(|instr| instr.to_wrapped_instruction())
+                            .collect::<Vec<_>>(),
+                        );
+                    }
+                    michelson::ast::Operation::MakeListOp { result } => {
+                        //スタック初期化の際に `nil ty` を既に積んでいるため何もする必要無し
+                        instructions.append(
+                            &mut vec![MichelsonInstruction::Comment(format!(
+                                "{} = michelson.make_list() {{ }}",
+                                result.get_value().get_id()
+                            ))]
+                            .iter()
+                            .map(|instr| instr.to_wrapped_instruction())
+                            .collect::<Vec<_>>(),
+                        )
+                    }
+                    michelson::ast::Operation::MakePairOp { result, fst, snd } => {
+                        let result_address = (*get_address_closure)(result.get_value());
+                        let fst_address = (*get_address_closure)(fst.get_value());
+                        let snd_address = (*get_address_closure)(snd.get_value());
+                        instructions.append(
+                            &mut vec![
+                                MichelsonInstruction::Comment(format!(
+                                    "{} = michelson.make_pair({}, {}) {{",
+                                    result.get_value().get_id(),
+                                    fst.get_value().get_id(),
+                                    snd.get_value().get_id(),
+                                )),
+                                MichelsonInstruction::DupN(snd_address),
+                                MichelsonInstruction::DupN(fst_address + 1),
+                                MichelsonInstruction::Pair,
+                                MichelsonInstruction::DigN(result_address),
+                                MichelsonInstruction::Drop,
+                                MichelsonInstruction::Some,
+                                MichelsonInstruction::DugN(result_address - 1),
+                                MichelsonInstruction::Comment("}".to_string()),
+                            ]
+                            .iter()
+                            .map(|instr| instr.to_wrapped_instruction())
+                            .collect::<Vec<_>>(),
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    instructions
+}
+
+///before: [value region] |> [heap region] |> stack_bottom
+///after :           (operations, storage) |> stack_bottom
+pub fn exit(
+    smart_contract: &Operation,
+    stack_size: usize,
+    get_address_closure: &(dyn Fn(Value) -> usize),
+) -> Vec<MWrappedInstr> {
+    let return_op = smart_contract.regions[0].blocks[0]
+        .operations
+        .last()
+        .unwrap()
+        .to_owned();
+    let value = return_op.operands[0].get_value();
+    let address = (*get_address_closure)(value);
+
+    let mut instructions = vec![instruction_row!(MichelsonInstruction::Comment(format!(
+        "-------------- Exit -------------- {{"
+    )))];
+
+    instructions.append(
+        &mut vec![
+            MichelsonInstruction::DigN(address - 1),
+            MichelsonInstruction::AssertSome,
+            MichelsonInstruction::DugN(stack_size - 1),
+        ]
+        .iter()
+        .map(|instr| instr.to_wrapped_instruction())
+        .collect::<Vec<_>>(),
+    );
+
+    for _ in 0..stack_size - 1 {
+        instructions.push(instruction_row!(MichelsonInstruction::Drop));
+    }
+
+    instructions.push(instruction_row!(MichelsonInstruction::Comment(format!(
+        "---------------------------------- }}"
+    ))));
+
+    instructions
+}
+
+pub fn get_get_address_closure(
+    value_addresses: HashMap<Value, usize>,
+) -> Box<dyn Fn(Value) -> usize> {
+    Box::new(move |value| *value_addresses.get(&value).unwrap())
+}
